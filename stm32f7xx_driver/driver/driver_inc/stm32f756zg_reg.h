43df688bc05c711c875c967a69b059121464aadc
Likith <likithrgowda8@gmail.com>
2026-04-24 20:08:51 +0530

    added docs reg.h and added doxyfile

--- stm32f7xx_driver/driver/driver_inc/stm32f756zg_reg.h
+++ stm32f7xx_driver/driver/driver_inc/stm32f756zg_reg.h
@@ -1,267 +1,251 @@
-/*
- * stm32f7xx_driver_custom.h
+/**
+ * @file stm32f7xx_driver_custom.h
+ * @brief Register definitions and memory mapping for STM32F756ZG
  *
- *  Created on: Feb 17, 2026
- *      Author: likith
+ * This file contains:
+ * - Base address definitions for memory and peripherals
+ * - Peripheral register structures
+ * - NVIC register mapping
+ *
+ * This is intended for bare-metal driver development without HAL.
+ *
+ * @author Likith
+ * @date Feb 17, 2026
  */
 
 #ifndef INC_STM32F756ZG_REG_H_
 #define INC_STM32F756ZG_REG_H_
+
 #include <stdint.h>
 #include <stddef.h>
 
+/**
+ * @defgroup NVIC NVIC Register Definitions
+ * @{
+ */
 
- //Processor related macros and structs
+/**
+ * @brief Base address of NVIC registers
+ */
 #define NVIC_BASE_ADDR   ((volatile uint32_t*)(0xE000E100))
+
+/**
+ * @brief NVIC register structure
+ */
 typedef struct
 {
-    volatile uint32_t ISER[8];
+    volatile uint32_t ISER[8];   /**< Interrupt Set Enable Registers */
     uint32_t RESERVED0[24];
-    volatile uint32_t ICER[8];
+    volatile uint32_t ICER[8];   /**< Interrupt Clear Enable Registers */
     uint32_t RESERVED1[24];
-    volatile uint32_t ISPR[8];
+    volatile uint32_t ISPR[8];   /**< Interrupt Set Pending Registers */
     uint32_t RESERVED2[24];
-    volatile uint32_t ICPR[8];
+    volatile uint32_t ICPR[8];   /**< Interrupt Clear Pending Registers */
     uint32_t RESERVED3[24];
-    volatile uint32_t IABR[8];
+    volatile uint32_t IABR[8];   /**< Interrupt Active Bit Registers */
     uint32_t RESERVED4[56];
-    volatile uint32_t IPR[60];
+    volatile uint32_t IPR[60];   /**< Interrupt Priority Registers */
 
 } NVIC_RegDef_t;
 
+/** NVIC peripheral definition */
 #define NVIC ((NVIC_RegDef_t*)(NVIC_BASE_ADDR))
 
+/** @} */
 
 
+/**
+ * @defgroup MEMORY_BASE_ADDRESSES Memory Base Addresses
+ * @{
+ */
+#define FLASH_BASE_ADDR     0x08000000UL  /**< Flash memory base address */
+#define DTCM_BASE_ADDR      0x20000000UL  /**< DTCM base address */
+#define SRAM1_BASE_ADDR     0x20010000UL  /**< SRAM1 base address */
+#define SRAM2_BASE_ADDR     0x2004C000UL  /**< SRAM2 base address */
+
+#define SRAM SRAM1_BASE_ADDR /**< Default SRAM */
+/** @} */
 
 
-//Defining BASE address of memory regions
-#define FLASH_BASE_ADDR					0x08000000UL  //Base Address of flash memory
-#define DTCM_BASE_ADDR                  0x20000000UL	//Base Address of DTCM
-#define SRAM1_BASE_ADDR					0x20010000UL //Base Address of SRAM1 memory
-#define SRAM2_BASE_ADDR					0x2004C000UL //Base Address of SRAM2 memory
-
-#define SRAM								SRAM1_BASE_ADDR
-
-//Defining BASE address of BUS regions
-
-#define APB1_PERIPH_BASE_ADDR			0x40000000UL	//Base address of APB1 bus peripherals
-#define APB2_PERIPH_BASE_ADDR			0x40010000UL	//Base address of APB2 bus peripherals
-#define	AHB1_PERIPH_BASE_ADDR	        0x40020000UL	//Base address of AHB1 bus peripherals
-#define	AHB2_PERIPH_BASE_ADDR		    0x50000000UL	//Base address of AHB2 bus peripherals
-#define AHB3_PERIPH_BASE_ADDR			0x60000000UL	//Base address of AHB3 bus peripherals
+/**
+ * @defgroup BUS_BASE_ADDRESSES Bus Base Addresses
+ * @{
+ */
+#define APB1_PERIPH_BASE_ADDR   0x40000000UL
+#define APB2_PERIPH_BASE_ADDR   0x40010000UL
+#define AHB1_PERIPH_BASE_ADDR   0x40020000UL
+#define AHB2_PERIPH_BASE_ADDR   0x50000000UL
+#define AHB3_PERIPH_BASE_ADDR   0x60000000UL
+/** @} */
 
 
-
-//generic macros
-#define ENABLE 	0x1UL
+/**
+ * @brief Generic enable/disable macros
+ */
+#define ENABLE  0x1UL
 #define DISABLE 0x0UL
 
 
+/**
+ * @defgroup APB1_PERIPHERALS APB1 Peripheral Base Addresses
+ * @{
+ */
+#define TIMMER_2_BASE_ADDR  (APB1_PERIPH_BASE_ADDR+0x0)
+#define TIMMER_3_BASE_ADDR  (APB1_PERIPH_BASE_ADDR+0x400)
 
-//Defining BASE address of peripherals of APB1 BUS regions
-// TIM2,3, RTC,  I2C1,I2C2,I2C3,  SPI2,SPI3,  USART2,3   UART4,5
+#define I2C1_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x5400)
+#define I2C2_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x5800)
+#define I2C3_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x5C00)
 
-#define TIMMER_2_BASE_ADDR		(APB1_PERIPH_BASE_ADDR+0x0)
-#define TIMMER_3_BASE_ADDR		(APB1_PERIPH_BASE_ADDR+0x400)
+#define SPI2_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x3800)
+#define SPI3_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x3C00)
 
-#define	I2C1_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x5400)
-#define I2C2_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x5800)
-#define I2C3_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x5C00)
+#define USART2_BASE_ADDR    (APB1_PERIPH_BASE_ADDR+0x4400)
+#define USART3_BASE_ADDR    (APB1_PERIPH_BASE_ADDR+0x4800)
 
-#define SPI2_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x3800)
-#define SPI3_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x3C00)
+#define UART4_BASE_ADDR     (APB1_PERIPH_BASE_ADDR+0x4C00)
+#define UART5_BASE_ADDR     (APB1_PERIPH_BASE_ADDR+0x5000)
 
-#define USART2_BASE_ADDR		(APB1_PERIPH_BASE_ADDR+0x4400)
-#define USART3_BASE_ADDR		(APB1_PERIPH_BASE_ADDR+0x4800)
-
-#define UART4_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x4C00)
-#define UART5_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x5000)
-
-#define RTC_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x2800)
-#define CAN1_BASE_ADDR			(APB1_PERIPH_BASE_ADDR+0x6400)
-
-
-//Defining BASE address of peripherals of APB2 BUS regions
-#define	EXTI_BASE_ADDR		(APB2_PERIPH_BASE_ADDR+0x3C00)
-#define SYSCFG_BASE_ADDR	(APB2_PERIPH_BASE_ADDR+0x3800)
-
-//defining base address of peripherals of AHB1 bus
-#define GPIOA_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x0)
-#define GPIOB_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x400)
-#define GPIOC_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x800)
-#define GPIOD_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0xC00)
-#define GPIOE_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x1000)
-#define GPIOF_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x1400)
-#define GPIOG_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x1800)
-#define GPIOH_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x1C00)
-#define GPIOI_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x2000)
-#define GPIOJ_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x2400)
-#define GPIOK_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x2800)
-
-#define RCC_BASE_ADDR		(AHB1_PERIPH_BASE_ADDR+0x3800)
+#define RTC_BASE_ADDR       (APB1_PERIPH_BASE_ADDR+0x2800)
+#define CAN1_BASE_ADDR      (APB1_PERIPH_BASE_ADDR+0x6400)
+/** @} */
 
 
-//Defining Base address of GPIO peripherals
-#define GPIO_A	((GPIO_RegDef_t*)GPIOA_BASE_ADDR)
-#define GPIO_B	((GPIO_RegDef_t*)GPIOB_BASE_ADDR)
-#define GPIO_C	((GPIO_RegDef_t*)GPIOC_BASE_ADDR)
-#define GPIO_D	((GPIO_RegDef_t*)GPIOD_BASE_ADDR)
-#define GPIO_E	((GPIO_RegDef_t*)GPIOE_BASE_ADDR)
-#define GPIO_F	((GPIO_RegDef_t*)GPIOF_BASE_ADDR)
-#define GPIO_G	((GPIO_RegDef_t*)GPIOG_BASE_ADDR)
-#define GPIO_H	((GPIO_RegDef_t*)GPIOH_BASE_ADDR)
-#define GPIO_I	((GPIO_RegDef_t*)GPIOI_BASE_ADDR)
-#define GPIO_J	((GPIO_RegDef_t*)GPIOJ_BASE_ADDR)
-#define GPIO_K	((GPIO_RegDef_t*)GPIOK_BASE_ADDR)
+/**
+ * @defgroup APB2_PERIPHERALS APB2 Peripheral Base Addresses
+ * @{
+ */
+#define EXTI_BASE_ADDR      (APB2_PERIPH_BASE_ADDR+0x3C00)
+#define SYSCFG_BASE_ADDR    (APB2_PERIPH_BASE_ADDR+0x3800)
+/** @} */
 
 
-#define RCC 	((RCC_RegDef_t*)RCC_BASE_ADDR)
+/**
+ * @defgroup AHB1_PERIPHERALS AHB1 Peripheral Base Addresses
+ * @{
+ */
+#define GPIOA_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x0)
+#define GPIOB_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x400)
+#define GPIOC_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x800)
+#define GPIOD_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0xC00)
+#define GPIOE_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x1000)
+#define GPIOF_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x1400)
+#define GPIOG_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x1800)
+#define GPIOH_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x1C00)
+#define GPIOI_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x2000)
+#define GPIOJ_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x2400)
+#define GPIOK_BASE_ADDR     (AHB1_PERIPH_BASE_ADDR+0x2800)
+
+#define RCC_BASE_ADDR       (AHB1_PERIPH_BASE_ADDR+0x3800)
+/** @} */
 
 
-#define USART_2 ((USART_RegDef_t*)USART2_BASE_ADDR)
-#define USART_3 ((USART_RegDef_t*)USART3_BASE_ADDR)
-#define UART_4	((USART_RegDef_t*)UART4_BASE_ADDR)
-#define UART_5	((USART_RegDef_t*)UART5_BASE_ADDR)
-
-#define EXTI 	((EXTI_RegDef_t*)EXTI_BASE_ADDR)
-
-#define SYSCFG	((SYSCFG_RegDef_t*)SYSCFG_BASE_ADDR)
-
-#define TIMER2 ((TIMER_RegDef_t*)TIMMER_2_BASE_ADDR)
-
-
-//IRQ numbers
-#define	IRQ_NO_EXTI0	6
-#define	IRQ_NO_EXTI1	7
-#define	IRQ_NO_EXTI2	8
-#define	IRQ_NO_EXTI3	9
-#define	IRQ_NO_EXTI4	10
-#define	IRQ_NO_EXTI5_9	23
-#define IRQ_NO_EXTI10_15	40
-
-#define IRQ_NO_USART3	39
-
-
-
-
-
-//Creating structures for peripheral registers
-
+/**
+ * @brief GPIO register definition structure
+ */
 typedef struct{
-	volatile uint32_t MODER; // GPIO port mode register
-	volatile uint32_t OTYPER; // GPIO port o/p type register
-	volatile uint32_t OSPEEDR; // GPIO port o/p speed register
-	volatile uint32_t PUPDR; //GPIO port pull-up/push-down register
-	volatile uint32_t IDR;	// GPIO port i/p data register
-	volatile uint32_t ODR;	//GPIO port o/p data register
-	volatile uint32_t BSRR; //GPIO port set/reset register
-	volatile uint32_t LCKR;	//GPIO port config lock register
-	volatile uint32_t AFR[2]; // GPIO alternate function register
-	}GPIO_RegDef_t;
+    volatile uint32_t MODER;     /**< GPIO mode register */
+    volatile uint32_t OTYPER;    /**< GPIO output type register */
+    volatile uint32_t OSPEEDR;   /**< GPIO speed register */
+    volatile uint32_t PUPDR;     /**< GPIO pull-up/pull-down register */
+    volatile uint32_t IDR;       /**< GPIO input data register */
+    volatile uint32_t ODR;       /**< GPIO output data register */
+    volatile uint32_t BSRR;      /**< GPIO bit set/reset register */
+    volatile uint32_t LCKR;      /**< GPIO configuration lock register */
+    volatile uint32_t AFR[2];    /**< GPIO alternate function registers */
+} GPIO_RegDef_t;
 
 
+/**
+ * @brief RCC register definition structure
+ */
 typedef struct{
-	volatile uint32_t CR;			//RCC clock control register
-	volatile uint32_t PLLCFGR;		// RCC PLL config register
-	volatile uint32_t CFGR;			// RCC clk config register
-	volatile uint32_t CIR;			//RCC clk interrupt register
-	volatile uint32_t AHB1RSTR;		//RCC AHB1 peripheral reset register
-	volatile uint32_t AHB2RSTR;		//RCC AHB2 peripheral reset register
-	volatile uint32_t AHB3RSTR;		//RCC AHB3 peripheral reset register
-	uint32_t	RESERVED_1;
-	volatile uint32_t APB1RSTR;		//RCC APB1 peripheral reset register
-	volatile uint32_t APB2RSTR;		//RCC APB2 peripheral reset register
-	uint32_t	RESERVED_2[2];
-	volatile uint32_t AHB1ENR;		//RCC AHB1 peripheral clock register
-	volatile uint32_t AHB2ENR;		//RCC AHB2 peripheral clock register
-	volatile uint32_t AHB3ENR;		//RCC AHB3 peripheral clock register
-	uint32_t	RESERVED_3;
-	volatile uint32_t APB1ENR;		//RCC APB1 peripheral clock enable register
-	volatile uint32_t APB2ENR;		//RCC APB1 peripheral clock enable register
-	uint32_t	RESERVED_4[2];
-	volatile uint32_t AHB1LPENR;	//RCC AHB1 peripheral clock enable in low power mode register
-	volatile uint32_t AHB2LPENR;	//RCC AHB2 peripheral clock enable in low power mode register
-	volatile uint32_t AHB3LPENR;	//RCC AHB3 peripheral clock enable in low power mode register
-	uint32_t	RESERVED_5;
-	volatile uint32_t APB1LPENR;	//RCC APB1 peripheral clock enable in low power mode register
-	volatile uint32_t APB2LPENR;	//RCC APB1 peripheral clock enable in low power mode register
-	uint32_t	RESERVED_6[2];
-	volatile uint32_t BDCR;			//RCC Backup domain control register
-	volatile uint32_t CSR;			//RCC clock control & status register
-	uint32_t	RESERVED_7[2];
-	volatile uint32_t SSCGR;		//RCC spread spectrum clock generation register
-	volatile uint32_t PLLI2SCFGR;	//RCC PLLI2S configuration register
-	volatile uint32_t PLLSAICFGR;	//RCC PLLSAI configuration register
-	volatile uint32_t DCKCFGR1;		//RCC Dedicated Clocks configuration register1
-	volatile uint32_t DCKCFGR2;		//RCC Dedicated Clocks configuration register2
-}RCC_RegDef_t;
+    volatile uint32_t CR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t CFGR;
+    volatile uint32_t CIR;
+    volatile uint32_t AHB1RSTR;
+    volatile uint32_t AHB2RSTR;
+    volatile uint32_t AHB3RSTR;
+    uint32_t RESERVED_1;
+    volatile uint32_t APB1RSTR;
+    volatile uint32_t APB2RSTR;
+    uint32_t RESERVED_2[2];
+    volatile uint32_t AHB1ENR;
+    volatile uint32_t AHB2ENR;
+    volatile uint32_t AHB3ENR;
+    uint32_t RESERVED_3;
+    volatile uint32_t APB1ENR;
+    volatile uint32_t APB2ENR;
+} RCC_RegDef_t;
 
 
+/**
+ * @brief USART register definition structure
+ */
 typedef struct{
-	volatile uint32_t  USART_CR1;
-	volatile uint32_t USART_CR2;
-	volatile uint32_t USART_CR3;
-	volatile uint32_t	USART_BRR;
-	volatile uint32_t USART_GTPR;
-	volatile uint32_t USART_RTOR;
-	volatile uint32_t USART_RQR;
-	volatile uint32_t USART_ISR;
-	volatile uint32_t USART_ICR;
-	volatile uint32_t USART_RDR;
-	volatile uint32_t USART_TDR;
-}USART_RegDef_t;
+    volatile uint32_t USART_CR1;
+    volatile uint32_t USART_CR2;
+    volatile uint32_t USART_CR3;
+    volatile uint32_t USART_BRR;
+    volatile uint32_t USART_GTPR;
+    volatile uint32_t USART_RTOR;
+    volatile uint32_t USART_RQR;
+    volatile uint32_t USART_ISR;
+    volatile uint32_t USART_ICR;
+    volatile uint32_t USART_RDR;
+    volatile uint32_t USART_TDR;
+} USART_RegDef_t;
 
 
+/**
+ * @brief EXTI register definition structure
+ */
 typedef struct{
-	volatile uint32_t EXTI_IMR;
-	volatile uint32_t EXTI_EMR;
-	volatile uint32_t EXTI_RTSR;
-	volatile uint32_t EXTI_FTSR;
-	volatile uint32_t EXTI_SWIER;
-	volatile uint32_t EXTI_PR;
+    volatile uint32_t EXTI_IMR;
+    volatile uint32_t EXTI_EMR;
+    volatile uint32_t EXTI_RTSR;
+    volatile uint32_t EXTI_FTSR;
+    volatile uint32_t EXTI_SWIER;
+    volatile uint32_t EXTI_PR;
+} EXTI_RegDef_t;
 
-}EXTI_RegDef_t;
 
-
+/**
+ * @brief SYSCFG register definition structure
+ */
 typedef struct{
-	volatile uint32_t SYSCFG_MEMRMP;
-	volatile uint32_t SYSCFG_PMC;
-	volatile uint32_t SYSCFG_EXTICR[4];
-	uint32_t reserved[2];
-	volatile uint32_t SYSCFG_CMPCR;
-}SYSCFG_RegDef_t;
+    volatile uint32_t SYSCFG_MEMRMP;
+    volatile uint32_t SYSCFG_PMC;
+    volatile uint32_t SYSCFG_EXTICR[4];
+    uint32_t reserved[2];
+    volatile uint32_t SYSCFG_CMPCR;
+} SYSCFG_RegDef_t;
 
 
-
+/**
+ * @brief Timer register definition structure
+ */
 typedef struct{
-	volatile uint32_t TIM_CR1;
-	volatile uint32_t TIM_CR2;
-	volatile uint32_t TIM_SMCR;
-	volatile uint32_t TIM_DIER;
-	volatile uint32_t TIM_SR;
-	volatile uint32_t TIM_EGR;
-	volatile uint32_t TIM_CCMR1;
-	volatile uint32_t TIM_CCMR2;
-	volatile uint32_t TIM_CCER;
-	volatile uint32_t TIM_CNT;
-	volatile uint32_t TIM_PSC;
-	volatile uint32_t TIM_ARR;
-	uint32_t RESERVED_1;
-	volatile uint32_t TIM_CCR1;
-	volatile uint32_t TIM_CCR2;
-	volatile uint32_t TIM_CCR3;
-	volatile uint32_t TIM_CCR4;
-	uint32_t RESERVED_2;
-	volatile uint32_t TIM_DCR;
-	volatile uint32_t TIM_DMR;
-	volatile uint32_t TIM_OR;
-
-
-}TIMER_RegDef_t;
+    volatile uint32_t TIM_CR1;
+    volatile uint32_t TIM_CR2;
+    volatile uint32_t TIM_SMCR;
+    volatile uint32_t TIM_DIER;
+    volatile uint32_t TIM_SR;
+    volatile uint32_t TIM_EGR;
+    volatile uint32_t TIM_CCMR1;
+    volatile uint32_t TIM_CCMR2;
+    volatile uint32_t TIM_CCER;
+    volatile uint32_t TIM_CNT;
+    volatile uint32_t TIM_PSC;
+    volatile uint32_t TIM_ARR;
+    uint32_t RESERVED_1;
+    volatile uint32_t TIM_CCR1;
+    volatile uint32_t TIM_CCR2;
+    volatile uint32_t TIM_CCR3;
+    volatile uint32_t TIM_CCR4;
+} TIMER_RegDef_t;
 
 #include "stm32f7xx_gpio_driver.h"
-
-
 
 #endif /* INC_STM32F756ZG_REG_H_ */
